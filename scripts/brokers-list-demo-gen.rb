5713905886b551ffb7126187b73d1c480f73afe1
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-08-23 18:23:03 +0200

    broke API into master & detail

--- scripts/brokers-list-demo-gen.rb
+++ scripts/brokers-list-demo-gen.rb
@@ -2,6 +2,15 @@
 
 require 'date'
 
+@example_no = 0
+def create_employer_example_file(content) 
+
+    path = "enroll/employer/employer_summary/response/example_#{@example_no}.json"
+    File.write("#{__dir__}/../#{path}", content)
+    @example_no += 1
+    "https://raw.githubusercontent.com/dchealthlink/HBX-mobile-app-APIs/master/#{path}"
+end
+
 class Date
     def on(mday)
         Date.new(year, mon, mday)
@@ -93,22 +102,29 @@ def office(first: "Primary", last: "Office", phone: "202-468-6571", email: "",
         city: city, state: state, zip: zip)
 end
 
-def participation(total, enrolled, waived)
+def participation(employer_name, total, enrolled, waived)
 
         ee_contrib = (enrolled * 425.00 * ((waived + 1) ** 0.2)).round(2)
         er_contrib = (enrolled * 770.00 * ((waived + 1) ** 0.22)).round(2)
 
-         """\"binder_payment_due\": null,
+        details_url = create_employer_example_file("""
+{
+    \"employer_name\": \"#{employer_name}\",
+    \"billing_report_date\": #{fmt(now >> 1)},
+    \"total_premium\": #{ee_contrib + er_contrib},
+    \"employee_contribution\": #{ee_contrib},
+    \"employer_contribution\": #{er_contrib}
+}""")
+
+         """\"employer_name\": \"#{employer_name}\",
+            \"employer_details_url\": \"#{details_url}\",
+            \"binder_payment_due\": null,
             \"employees_total\": #{total},
             \"employees_enrolled\": #{enrolled},
             \"employees_waived\": #{waived},
-            \"minimum_participation_required\": #{(total * 2.0 / 3.0).to_i},
-            \"estimated_premium\": {
-                \"billing_report_date\": #{fmt(now >> 1)},
-                \"total_premium\": #{ee_contrib + er_contrib},
-                \"employee_contribution\": #{ee_contrib},
-                \"employer_contribution\": #{er_contrib}
-                }"""
+            \"minimum_participation_required\": #{(total * 2.0 / 3.0).to_i}"""
+
+          
 end
 
 
@@ -127,10 +143,9 @@ text = """
     
     \"broker_clients\": [
             {
-            \"employer_name\": \"Courageous Consulting, LLC\",
+            #{participation("Courageous Consulting, LLC", 30, 5, 0)},
             #{late_open_enrollment.date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(30, 5, 0)},
             \"contact_info\": [
                 #{staffer(first: "Bob", phone: "", email: "bob@courageous-consulting.com")},
                 #{office(address_1: "1600 Pennsylvania Avenue")}
@@ -138,21 +153,18 @@ text = """
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"National Network to End Domestic Abuse\",
+            #{participation("National Network to End Domestic Abuse", 41, 10, 5)},
             #{in_open_enrollment.date_fields},
-            \"renewal_in_progress\": \"false\",            
-            #{participation(41, 10, 5)},
-            \"contact_info\": [
+            \"renewal_in_progress\": \"false\", \"contact_info\": [
                 #{staffer(first: "Jane", phone: "202-555-0000", email: "contact@endabuse.org")},
                 #{office(address_1: "1600 New Hampshire Avenue")}
             ],
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"District Yoga\",
+            #{participation("District Yoga", 30, 20, 3)},
             #{in_open_enrollment.date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(30, 20, 3)},
             \"contact_info\": [      
                 #{staffer(first: "Priya", last: "Chandragupta", email: "contact@districtyoga.com")},
                 #{office(address_1: "1600 New York Avenue", phone: "202-555-0212")}
@@ -160,10 +172,9 @@ text = """
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"DC Cupcakes\",
+            #{participation("DC Cupcakes", 50, 40, 7)},
             #{plan_starting_in(5, false).date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(50, 40, 7)},
             \"contact_info\": [      
                 #{staffer(first: "Emile", last: "Della Noce", email: "contact@dccupcakes.com")},
                 #{office(address_1: "1600 Rhode Island Avenue", phone: "202-555-0313")}
@@ -171,10 +182,9 @@ text = """
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"OPEN Art Studio\",
+            #{participation("OPEN Art Studio", 30, 20, 3)},
             #{late_open_enrollment.date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(30, 20, 3)},
             \"contact_info\": [      
                 #{staffer(first: "Yona", last: "Mendelssohn", email: "yona@openart.org")},
                 #{staffer(first: "Noam", last: "Mendelssohn", email: "noam@openart.org")},
@@ -183,10 +193,9 @@ text = """
             \"active_general_agency\": \"Betadyne General Agency, Inc.\"
             },
             {
-            \"employer_name\": \"Best Brau Brewing Company\",
+            #{participation("Best Brau Brewing Company", 30, 20, 6)},
             #{in_renewal.date_fields},
             \"renewal_in_progress\": \"true\",
-            #{participation(30, 20, 6)},
             \"contact_info\": [      
                 #{staffer(first: "Heinrich", last: "Biergarten", email: "heini@bestbrau.com")},
                 #{staffer(first: "Ulrich", last: "Suufersohn", email: "ueli@bestbrau.com")},
@@ -196,10 +205,10 @@ text = """
             \"active_general_agency\": \"ACME General Agency, Inc.\"
             },
             {
-            \"employer_name\": \"Bistrot Du Monde\",
+            #{participation("Bistrot Du Monde", 33, 20, 4)},
+            \"employer_name\": \"\",
             #{late_to_renewal.date_fields},
             \"renewal_in_progress\": \"true\",
-            #{participation(33, 20, 4)},
             \"contact_info\": [      
                 #{staffer(first: "Claudette", last: "Noire", email: "cnoire@dumonde.com")},
                 #{staffer(first: "Aloise", last: "Rouge", email: "arouge@dumonde.com")},
@@ -208,10 +217,9 @@ text = """
             \"active_general_agency\": \"ACME General Agency, Inc.\"
             },  
             {
-            \"employer_name\": \"Bistrot Du Bois\",
+            #{participation("Bistrot Du Bois", 15, 7, 7)},
             #{early_renewal.date_fields},
             \"renewal_in_progress\": \"true\",
-            #{participation(15, 7, 7)},
             \"contact_info\": [      
                 #{staffer(first: "Claudette", last: "Blanc", email: "cblan@dubois.com")},
                 #{staffer(first: "Aloise", last: "Vert", email: "avert@dubois.com")},
@@ -220,10 +228,9 @@ text = """
             \"active_general_agency\": \"ACME General Agency, Inc.\"
             },
             {
-            \"employer_name\": \"Strategy & Tactics Game Shop\",
+            #{participation("Strategy & Tactics Game Shop", 6, 2, 2)},
             #{plan_starting_in(8, false).date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(6, 2, 2)},
             \"contact_info\": [      
                 #{staffer(first: "Maria Susanna", last: "Ludador", email: "contact@stgames.com")},
                 #{office(address_1: "1600 Georgia Avenue", phone: "202-555-0007")}
@@ -231,10 +238,9 @@ text = """
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"Portia's Tea Bar\",
+            #{participation("Portia's Tea Bar", 12, 9, 2)},
             #{plan_starting_in(7, false).date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(12, 9, 2)},
             \"contact_info\": [      
                 #{staffer(first: "Portia", last: "Binglesworth-Inglesham", email: "portia@helloportia.com")},
                 #{office(address_1: "1600 Alabama Avenue", phone: "202-555-0008")},
@@ -243,10 +249,9 @@ text = """
             \"active_general_agency\": null
             },
             {
-            \"employer_name\": \"J. Grigory Food Trucks & Fine Comestibles\",
+            #{participation("J. Grigory Food Trucks & Fine Comestibles", 66, 50, 0)},
             #{plan_starting_in(6, false).date_fields},
             \"renewal_in_progress\": \"false\",
-            #{participation(66, 50, 0)},
             \"contact_info\": [
                 #{staffer(first: "Joe", last: "Grigory", email: "jgrigory@jgrigory.com")},
                 #{office(address_1: "1600 North Carolina Avenue", phone: "202-555-0009")}
