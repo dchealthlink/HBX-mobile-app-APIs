2292f5f6763784fdb820419bdd292708e3d79c63
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-09-20 15:04:40 +0200

    more fields in details files

--- scripts/brokers-list-demo-gen.rb
+++ scripts/brokers-list-demo-gen.rb
@@ -26,9 +26,14 @@ class Date
 end
 
 class PlanYear
-    def initialize(start_date, in_open_enrollment)
+    def initialize(start_date, in_open_enrollment, in_pending_renewal)
         @start_date = start_date
         @in_open_enrollment = in_open_enrollment
+        @in_pending_renewal = in_pending_renewal
+    end
+
+    def in_pending_renewal
+        @in_pending_renewal
     end
 
     def end_of_open_enrollment
@@ -69,8 +74,8 @@ def now
     Date.parse(Time.now.to_s)
 end
 
-def plan_starting_in(months, in_open_enrollment)
-    PlanYear.new( (now >> months).on(1), in_open_enrollment)
+def plan_starting_in(months, in_open_enrollment = false, in_pending_renewal = false)
+    PlanYear.new( (now >> months).on(1), in_open_enrollment, in_pending_renewal)
 end
 
 def fmt_unescaped(dt)
@@ -118,7 +123,9 @@ def json_section(obj, opts)
     JSON.pretty_generate(obj, opts).gsub(/\{|\}/, '').rstrip
 end
 
-def participation(employer_name, total, enrolled, waived)
+def participation(employer_name, total, enrolled, waived, plan_year)
+
+    print "%%%%%% #{plan_year} %%%%%\n\n"
 
         ee_contrib = (enrolled * 425.00 * ((waived + 1) ** 0.2)).round(2)
         er_contrib = (enrolled * 770.00 * ((waived + 1) ** 0.22)).round(2)
@@ -128,10 +135,17 @@ def participation(employer_name, total, enrolled, waived)
             employees_total: total,
             employees_enrolled: enrolled,
             employees_waived: waived,
+            open_enrollment_begins:        fmt_unescaped(plan_year.open_enrollment_begins),
+            open_enrollment_ends:          fmt_unescaped(plan_year.end_of_open_enrollment),
+            plan_year_begins:              fmt_unescaped(plan_year.plan_year_begins),
+            renewal_in_progress:           plan_year.in_pending_renewal,
+            renewal_application_available: fmt_unescaped(plan_year.renewal_begins),
+            renewal_application_due:       fmt_unescaped(plan_year.renewal_deadline),
+            binder_payment_due: nil,
             minimum_participation_required: (total * 2.0 / 3.0).to_i,
             billing_report_date: fmt_unescaped(now >> 1),
-            binder_payment_due: nil,
-            active_general_agency: (total < 5) ? nil : "Betadyne General Agency, Inc."
+            active_general_agency: (total < 5) ? nil : "Betadyne General Agency, Inc.",
+
         }
 
         details = summary.clone
@@ -165,11 +179,11 @@ end
 min_months_to_renew = if now.mday > 10 then 2 else 1 end
 min_months_to_enroll = if now.mday > 13 then 2 else 1 end
 
-in_open_enrollment = plan_starting_in(2, true)
-late_open_enrollment = plan_starting_in(min_months_to_enroll, true)
-in_renewal = plan_starting_in(2, false)
-early_renewal  = plan_starting_in(3, false)
-late_to_renewal = plan_starting_in(min_months_to_renew, false)
+in_open_enrollment = plan_starting_in(2, true, false)
+late_open_enrollment = plan_starting_in(min_months_to_enroll, true, false)
+in_renewal = plan_starting_in(2, false, true)
+early_renewal  = plan_starting_in(3, false, true)
+late_to_renewal = plan_starting_in(min_months_to_renew, false, true)
 
 text = """
  {
@@ -177,44 +191,36 @@ text = """
     
     \"broker_clients\": [
             {
-            #{participation("Courageous Consulting, LLC", 30, 5, 0)},
-            #{late_open_enrollment.date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("Courageous Consulting, LLC", 30, 5, 0, late_open_enrollment)},
+            
             \"contact_info\": [
                 #{staffer(first: "Bob", phone: "", email: "bob@courageous-consulting.com")},
                 #{office(address_1: "1600 Pennsylvania Avenue")}
             ]
             },
             {
-            #{participation("National Network to End Domestic Abuse", 41, 10, 5)},
-            #{in_open_enrollment.date_fields},
-            \"renewal_in_progress\": \"false\", \"contact_info\": [
+            #{participation("National Network to End Domestic Abuse", 41, 10, 5, in_open_enrollment)},
+            \"contact_info\": [
                 #{staffer(first: "Jane", phone: "202-555-0000", email: "contact@endabuse.org")},
                 #{office(address_1: "1600 New Hampshire Avenue")}
             ]
             },
             {
-            #{participation("District Yoga", 30, 20, 3)},
-            #{in_open_enrollment.date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("District Yoga", 30, 20, 3, in_open_enrollment)},
             \"contact_info\": [      
                 #{staffer(first: "Priya", last: "Chandragupta", email: "contact@districtyoga.com")},
                 #{office(address_1: "1600 New York Avenue", phone: "202-555-0212")}
             ]
             },
             {
-            #{participation("DC Cupcakes", 50, 40, 7)},
-            #{plan_starting_in(5, false).date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("DC Cupcakes", 50, 40, 7, plan_starting_in(5, false))},
             \"contact_info\": [      
                 #{staffer(first: "Emile", last: "Della Noce", email: "contact@dccupcakes.com")},
                 #{office(address_1: "1600 Rhode Island Avenue", phone: "202-555-0313")}
             ]
             },
             {
-            #{participation("OPEN Art Studio", 30, 20, 3)},
-            #{late_open_enrollment.date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("OPEN Art Studio", 30, 20, 3, late_open_enrollment)},
             \"contact_info\": [      
                 #{staffer(first: "Yona", last: "Mendelssohn", email: "yona@openart.org")},
                 #{staffer(first: "Noam", last: "Mendelssohn", email: "noam@openart.org")},
@@ -222,9 +228,7 @@ text = """
             ]
             },
             {
-            #{participation("Best Brau Brewing Company", 30, 20, 6)},
-            #{in_renewal.date_fields},
-            \"renewal_in_progress\": \"true\",
+            #{participation("Best Brau Brewing Company", 30, 20, 6, in_renewal)},
             \"contact_info\": [      
                 #{staffer(first: "Heinrich", last: "Biergarten", email: "heini@bestbrau.com")},
                 #{staffer(first: "Ulrich", last: "Suufersohn", email: "ueli@bestbrau.com")},
@@ -233,10 +237,7 @@ text = """
             ]
             },
             {
-            #{participation("Bistrot Du Monde", 33, 20, 4)},
-            \"employer_name\": \"\",
-            #{late_to_renewal.date_fields},
-            \"renewal_in_progress\": \"true\",
+            #{participation("Bistrot Du Monde", 33, 20, 4, late_to_renewal)},
             \"contact_info\": [      
                 #{staffer(first: "Claudette", last: "Noire", email: "cnoire@dumonde.com")},
                 #{staffer(first: "Aloise", last: "Rouge", email: "arouge@dumonde.com")},
@@ -244,9 +245,7 @@ text = """
             ]
             },  
             {
-            #{participation("Bistrot Du Bois", 15, 7, 7)},
-            #{early_renewal.date_fields},
-            \"renewal_in_progress\": \"true\",
+            #{participation("Bistrot Du Bois", 15, 7, 7, early_renewal)},
             \"contact_info\": [      
                 #{staffer(first: "Claudette", last: "Blanc", email: "cblan@dubois.com")},
                 #{staffer(first: "Aloise", last: "Vert", email: "avert@dubois.com")},
@@ -254,18 +253,14 @@ text = """
             ]
             },
             {
-            #{participation("Strategy & Tactics Game Shop", 6, 2, 2)},
-            #{plan_starting_in(8, false).date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("Strategy & Tactics Game Shop", 6, 2, 2, plan_starting_in(8, false, false))},
             \"contact_info\": [      
                 #{staffer(first: "Maria Susanna", last: "Ludador", email: "contact@stgames.com")},
                 #{office(address_1: "1600 Georgia Avenue", phone: "202-555-0007")}
             ]
             },
             {
-            #{participation("Portia's Tea Bar", 12, 9, 2)},
-            #{plan_starting_in(7, false).date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("Portia's Tea Bar", 12, 9, 2, plan_starting_in(7, false, false))},
             \"contact_info\": [      
                 #{staffer(first: "Portia", last: "Binglesworth-Inglesham", email: "portia@helloportia.com")},
                 #{office(address_1: "1600 Alabama Avenue", phone: "202-555-0008")},
@@ -273,9 +268,7 @@ text = """
             ]
             },
             {
-            #{participation("J. Grigory Food Trucks & Fine Comestibles", 66, 50, 0)},
-            #{plan_starting_in(6, false).date_fields},
-            \"renewal_in_progress\": \"false\",
+            #{participation("J. Grigory Food Trucks & Fine Comestibles", 66, 50, 0, plan_starting_in(6, false, false))},
             \"contact_info\": [
                 #{staffer(first: "Joe", last: "Grigory", email: "jgrigory@jgrigory.com")},
                 #{office(address_1: "1600 North Carolina Avenue", phone: "202-555-0009")}
