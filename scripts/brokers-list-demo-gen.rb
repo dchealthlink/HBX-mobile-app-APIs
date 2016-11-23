18b431ce64650f9088877856075a11da70e55fb2
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-11-23 14:21:06 +0100

    Revert "standardized use of JSON.pretty_print"
    This reverts commit d90b570b14ec67e6bb1ac5f4486e6e816dfcdf7e.

--- scripts/brokers-list-demo-gen.rb
+++ scripts/brokers-list-demo-gen.rb
@@ -216,11 +216,11 @@ def participation(employer_name, total, enrolled, waived, plan_year, contacts)
 
 
   summary = {
-    employer_name:                  "#{employer_name}", 
-    binder_payment_due:             nil,
+    employer_name:                 "#{employer_name}", 
+    binder_payment_due: nil,
     minimum_participation_required: (total_employees * 2.0 / 3.0).to_i,
-    billing_report_date:            fmt(now >> 1),
-    active_general_agency:          (total_employees < 5) ? nil : "Betadyne General Agency, Inc.",
+    billing_report_date: fmt(now >> 1),
+    active_general_agency: (total_employees < 5) ? nil : "Betadyne General Agency, Inc.",
   }
   details = summary.clone
 
@@ -318,7 +318,7 @@ def participation(employer_name, total, enrolled, waived, plan_year, contacts)
   summary[:employer_details_url] = create_employer_details_example_file(details)
   summary[:employee_roster_url] = create_employee_roster_example_file(roster)
   summary[:contact_info] = contacts
-  summary  
+  JSON.pretty_generate(summary, {indent: "            "})       
 end
 
 
@@ -331,48 +331,52 @@ in_renewal = plan_starting_in(2, false, true)
 early_renewal  = plan_starting_in(3, false, true)
 late_to_renewal = plan_starting_in(min_months_to_renew, false, true)
 
-result = {
-    broker_agency: "Bob's Brokers",
-    broker_name: "Bill",
+text = """
+ {
+    \"broker_agency\" : \"Bob's Brokers\",
+    \"broker_name\" : \"Bill\",
     
-    broker_clients: [
-            participation("Courageous Consulting, LLC", 30, 5, 0, late_open_enrollment,
+    \"broker_clients\": [
+            #{participation("Courageous Consulting, LLC", 30, 5, 0, late_open_enrollment,
                [staffer(first: "Bob", phone: "", email: "bob@courageous-consulting.com"),
-                ]),
-            participation("National Network to End Domestic Abuse", 41, 10, 5, in_open_enrollment, [staffer(first: "Jane", phone: "202-555-0000", email: "contact@endabuse.org"), office(address_1: "1600 New Hampshire Avenue")]),
-            participation("District Yoga", 30, 20, 3, in_open_enrollment, [staffer(first: "Priya", last: "Chandragupta", email: "contact@districtyoga.com"),office(address_1: "1600 New York Avenue", phone: "202-555-0212")]),
-            participation("DC Cupcakes", 50, 40, 7, plan_starting_in(5), [
+                ])},
+            #{participation("National Network to End Domestic Abuse", 41, 10, 5, in_open_enrollment, [staffer(first: "Jane", phone: "202-555-0000", email: "contact@endabuse.org"), office(address_1: "1600 New Hampshire Avenue")])},
+            #{participation("District Yoga", 30, 20, 3, in_open_enrollment, [staffer(first: "Priya", last: "Chandragupta", email: "contact@districtyoga.com"),office(address_1: "1600 New York Avenue", phone: "202-555-0212")])},
+            #{participation("DC Cupcakes", 50, 40, 7, plan_starting_in(5), [
                 staffer(first: "Emile", last: "Della Noce", email: "contact@dccupcakes.com"), 
-                office(address_1: "1600 Rhode Island Avenue", phone: "202-555-0313")]),
-            participation("OPEN Art Studio", 30, 20, 3, late_open_enrollment, [
+                office(address_1: "1600 Rhode Island Avenue", phone: "202-555-0313")])}
+            #{participation("OPEN Art Studio", 30, 20, 3, late_open_enrollment, [
                 staffer(first: "Yona", last: "Mendelssohn", email: "yona@openart.org"),
                 staffer(first: "Noam", last: "Mendelssohn", email: "noam@openart.org"),
-                office(address_1: "1600 Arizona Avenue", phone: "202-555-0414")]),
-            participation("Best Brau Brewing Company", 30, 20, 6, in_renewal, [
+                office(address_1: "1600 Arizona Avenue", phone: "202-555-0414")])}
+            #{participation("Best Brau Brewing Company", 30, 20, 6, in_renewal, [
                 staffer(first: "Heinrich", last: "Biergarten", email: "heini@bestbrau.com"),
                 staffer(first: "Ulrich", last: "Suufersohn", email: "ueli@bestbrau.com"),
                 office(address_1: "1600 Nebraska Avenue", phone: "202-555-0515"),
-                office(first: "Branch", address_1: "6600 Nebraska Avenue")]),
-            participation("Bistrot Du Monde", 33, 20, 4, late_to_renewal, [
+                office(first: "Branch", address_1: "6600 Nebraska Avenue")])}
+            #{participation("Bistrot Du Monde", 33, 20, 4, late_to_renewal, [
                 staffer(first: "Claudette", last: "Noire", email: "cnoire@dumonde.com"),
                 staffer(first: "Aloise", last: "Rouge", email: "arouge@dumonde.com"),
-                office(address_1: "1600 Louisiana Avenue", phone: "202-555-0005")]),
-            participation("Bistrot Du Bois", 15, 7, 7, early_renewal, [
+                office(address_1: "1600 Louisiana Avenue", phone: "202-555-0005")])} 
+            
+            #{participation("Bistrot Du Bois", 15, 7, 7, early_renewal, [
                 staffer(first: "Claudette", last: "Blanc", email: "cblan@dubois.com"),
                 staffer(first: "Aloise", last: "Vert", email: "avert@dubois.com"),
-                office(address_1: "1600 Maine Avenue", phone: "202-555-0006")]),
-            participation("Strategy & Tactics Game Shop", 6, 2, 2, plan_starting_in(8), [
+                office(address_1: "1600 Maine Avenue", phone: "202-555-0006")])}
+            #{participation("Strategy & Tactics Game Shop", 6, 2, 2, plan_starting_in(8), [
                 staffer(first: "Maria Susanna", last: "Ludador", email: "contact@stgames.com"),
-                office(address_1: "1600 Georgia Avenue", phone: "202-555-0007")]),
-            participation("Portia's Tea Bar", 12, 9, 2, plan_starting_in(7), [
+                office(address_1: "1600 Georgia Avenue", phone: "202-555-0007")])}
+            #{participation("Portia's Tea Bar", 12, 9, 2, plan_starting_in(7), [
                 staffer(first: "Portia", last: "Binglesworth-Inglesham", email: "portia@helloportia.com"),
                 office(address_1: "1600 Alabama Avenue", phone: "202-555-0008"),
-                office(first: "Branch", address_1: "1600 Utah Avenue", address_2: "Suite 500", phone: "202-555-0009")]),
-            participation("J. Grigory Food Trucks & Fine Comestibles", 66, 50, 0, plan_starting_in(6), [staffer(first: "Joe", last: "Grigory", email: "jgrigory@jgrigory.com"),
-                office(address_1: "1600 North Carolina Avenue", phone: "202-555-0009")])
+                office(first: "Branch", address_1: "1600 Utah Avenue", address_2: "Suite 500", phone: "202-555-0009")])}
+            #{participation("J. Grigory Food Trucks & Fine Comestibles", 66, 50, 0, plan_starting_in(6), [staffer(first: "Joe", last: "Grigory", email: "jgrigory@jgrigory.com"),
+                office(address_1: "1600 North Carolina Avenue", phone: "202-555-0009")])}
     ]
 }
-print JSON.pretty_generate(result)     
+"""
+
+print text
 
 
 
