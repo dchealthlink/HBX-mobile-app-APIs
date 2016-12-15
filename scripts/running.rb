fe037c391418c8818f31565259f266c21e24f131
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-12-15 15:21:58 +0100

    correct directory

--- scripts/running.rb
+++ scripts/running.rb
@@ -6,6 +6,6 @@ hash = Hash[procs.map do |p|
 end]
 hash.each do |env, line| 
 	loc = `grep -az "\\bPWD" #{env}`.gsub(/^PWD=/, "").gsub("\u0000", '') 
-	branch = `cd #{loc}/..; git status 2>&1`.lines[0]
+	branch = `cd #{loc}; git status 2>&1`.lines[0]
 	print "#{line}\nrunning in: #{loc}\n#{branch}\n"
 end
