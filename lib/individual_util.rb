ea2a0d6e2fa3a7eb430f666d6f7651830240001e
Krish Palaniappan <krish@snowpal.software>
2017-03-20 17:53:18 -0400

    Made enroll_waived_or_terminated() DRY-er.

--- lib/individual_util.rb
+++ lib/individual_util.rb
@@ -50,25 +50,12 @@ class IndividualUtil < InsuredUtil
 
   def enroll_waived_or_terminated enrollment, status
     enrollment[:total_premium] = cost status
-    enrollment[:plan_name] = 'KP DC Silver 2000/35'
-    enrollment[:plan_type] = 'HMO'
-    enrollment[:metal_level] = 'Silver'
     enrollment[:elected_aptc_pct] = 0
     enrollment[:applied_aptc_amount_in_cents] = 0
 
-    enrollment[:carrier] = {
-        name: 'Kaiser',
-        summary_of_benefits_url: '/document/download/dchbx-enroll-sbc-preprod/ad954b2b-81ca-4729-b440-811eead43498?content_type=application/pdf&filename=UHCChoicePlusHSAPOSGold1300A.pdf&disposition=inline'
-    }
-
-    enrollment[:provider_directory_url] = 'http://mydoctor.kaiserpermanente.org/mas/mdo/?kp_shortcut_referrer=kp.org/doctor'
-    enrollment[:rx_formulary_url] = 'https://healthy.kaiserpermanente.org/static/health/pdfs/formulary/mid/mid_exchange_formulary.pdf'
-    enrollment[:services_rates_url] = BaseUtil::url "#{$GENERATED_DIR}/#{ServiceUtil::SERVICES_RATE_JSON}"
-
-    if status == 'Terminated'
-      enrollment[:terminated_on] = Date.today
-      enrollment[:terminate_reason] = 'I have coverage through an individual market health plan'
-    end
+    add_plan_details! enrollment
+    add_urls! enrollment
+    add_terminated! enrollment, status
   end
 
   def cost status
