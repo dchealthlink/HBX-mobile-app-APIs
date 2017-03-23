b43ccc0c20d339d41b32597297ac24690b455722
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2017-03-23 14:13:40 +0100

    corrected provider & rx urls in plans

--- lib/plan_builder.rb
+++ lib/plan_builder.rb
@@ -14,14 +14,46 @@ class PlanBuilder
     (0..length).map { random_hex_char }.join ''
   end
 
+  def create_bluecross_standard_hmo_health num_members, deductible: 5000, metal_level: 'bronze', avg_member_premium: 250.00
+    create_bluecross_health "BlueChoice HMO Standard #{metal_level.capitalize} $#{deductible}", num_members,
+      avg_member_premium: avg_member_premium, deductible: deductible, metal_level: metal_level,
+      provider_directory_url: 'https://member.carefirst.com/mos/#/fadpublic/search/standard?sType=M&planCode=DC_CC3&planName=BlueChoice%20HMO&isNational=N'
+  end
+
+  def create_bluecross_ppo_health num_members, deductible: 5000, metal_level: 'bronze', avg_member_premium: 250.00
+    create_bluecross_health "BluePreferred PPO Standard #{metal_level.capitalize} $#{deductible}", 
+      num_members, avg_member_premium: avg_member_premium, deductible: deductible, 
+      metal_level: metal_level, plan_type: 'ppo', dc_in_network: false, nationwide: true,
+      provider_directory_url: 'https://member.carefirst.com/mos/#/fadpublic/search/standard?sType=M&planCode=DC_SP1&planName=Blue%20Preferred&isNational=Y'
+  end
+
+  def create_bluecross_health name, num_members, avg_member_premium: 250.00, dc_in_network: true, 
+                  deductible: 5000, is_standard_plan: true, metal_level: 'bronze', nationwide: false,
+                  plan_type: 'hmo', provider_directory_url: nil
+    create_plan name, num_members, deductible: deductible, dc_in_network: dc_in_network,
+      avg_member_premium: avg_member_premium, is_standard_plan: is_standard_plan, 
+      metal_level: metal_level, nationwide: nationwide, plan_type: plan_type, 
+      provider_directory_url: provider_directory_url, 
+      rx_formulary_url: 'https://carefirst.com/acarx'
+  end
+
+  def create_kaiser_health name, num_members, avg_member_premium: 250.00, 
+                  deductible: 5000, is_standard_plan: true, metal_level: 'bronze'
+    create_plan name, num_members, deductible: deductible, 
+      avg_member_premium: avg_member_premium, is_standard_plan: is_standard_plan, 
+      metal_level: metal_level,  
+      provider_directory_url: 'http://mydoctor.kaiserpermanente.org/mas/mdo/?kp_shortcut_referrer=kp.org/doctor', 
+      rx_formulary_url: 'https://healthy.kaiserpermanente.org/static/health/pdfs/formulary/mid/mid_exchange_formulary.pdf'
+  end
+
   def create_plan name,  num_members,
                   deductible: 5000, 
                   dc_in_network: true, coverage_kind: 'health',
                   dental_level: '', is_standard_plan: true,
                   metal_level: 'bronze', nationwide: false,
                   plan_type: 'hmo', avg_member_premium: 250.00,
-                  provider_directory_url: 'https://member.carefirst.com/mos/#/fadpublic/search/standard?sType=M&planCode=DC_CC3&planName=BlueChoice%20HMO&isNational=N',
-                  rx_formulary_url: 'https://carefirst.com/acarx',
+                  provider_directory_url: nil,
+                  rx_formulary_url: nil,
                   active_year: 2017
 
     single_deductible = in_dollars deductible
--- lib/plan_util.rb
+++ lib/plan_util.rb
@@ -24,67 +24,65 @@ class PlanUtil < BaseUtil
   	builder = ::PlanBuilder.new fixed_shuffler
 
   	[
-              builder.create_plan('BlueChoice HMO Standard Bronze $5000',            num_members,
+              builder.create_bluecross_standard_hmo_health(num_members,
               	deductible: 5000, metal_level: 'bronze', avg_member_premium: 220.1),
               
-              builder.create_plan('BlueChoice HMO Standard Gold $500',                num_members,                   
+              builder.create_bluecross_standard_hmo_health(num_members,                   
                deductible: 500, metal_level: 'gold', avg_member_premium: 349.11),
               
-              builder.create_plan('BlueChoice HMO Standard Platinum $0',              num_members,                     
-               deductible: 0, metal_level: 'platinum', avg_member_premium: 414.41), 
+              builder.create_bluecross_standard_hmo_health(num_members,
+                deductible: 0, metal_level: 'platinum', avg_member_premium: 414.41), 
               
-              builder.create_plan('BlueChoice HMO Standard Silver $2000',            num_members,                     
+              builder.create_bluecross_standard_hmo_health(num_members,
                deductible: 2000, metal_level: 'silver', avg_member_premium: 267.78), 
               
-              builder.create_plan('BlueChoice HMO Young Adult $7,150',                num_members,
-              	deductible: 7150,   dc_in_network: false, is_standard_plan: false, metal_level: 'catastrophic',
-              	avg_member_premium: 113.28),
+              builder.create_bluecross_health('BlueChoice HMO Young Adult $7,150', num_members,
+              	deductible: 7150, dc_in_network: false, is_standard_plan: false, 
+              	metal_level: 'catastrophic', avg_member_premium: 113.28),
               
-              builder.create_plan('BluePreferred PPO Standard Bronze $5000',         num_members,
-              	deductible: 5000,   dc_in_network: false, metal_level: 'bronze', nationwide: true, plan_type: 'ppo',
-              	avg_member_premium: 224.76),   
+              builder.create_bluecross_ppo_health(num_members,
+              	deductible: 5000, metal_level: 'bronze', avg_member_premium: 224.76),   
               
-              builder.create_plan('BluePreferred PPO Standard Gold $500',             num_members,
-              	deductible: 500,     dc_in_network: false, metal_level: 'gold', nationwide: true, plan_type: 'ppo',
-              	avg_member_premium: 367.59),    
+              builder.create_bluecross_ppo_health(num_members,
+              	deductible: 500, metal_level: 'gold', avg_member_premium: 367.59),    
               
-              builder.create_plan('BluePreferred PPO Standard Platinum $0',           num_members,
-              	deductible: 0,       dc_in_network: false, metal_level: 'platinum', nationwide: true, plan_type: 'ppo',
-              	avg_member_premium: 427.9),             
-              builder.create_plan('BluePreferred PPO Standard Silver $2000',         num_members,
-              	deductible: 2000, metal_level: 'silver', nationwide: true, plan_type: 'ppo', avg_member_premium: 283.46),    
+              builder.create_bluecross_ppo_health(num_members,
+              	deductible: 0, metal_level: 'platinum', avg_member_premium: 427.9), 
+
+              builder.create_bluecross_ppo_health(num_members,
+              	deductible: 2000, metal_level: 'silver', avg_member_premium: 283.46),    
               
-              builder.create_plan('KP DC Bronze 6200/20%/HSA/Dental/Ped Dental',      num_members,
+              builder.create_kaiser_health('KP DC Bronze 6200/20%/HSA/Dental/Ped Dental', num_members,
               	deductible: 6200, is_standard_plan: false, metal_level: 'bronze', avg_member_premium: 203.11),    
               
-              builder.create_plan('KP DC Bronze 6400/55/Dental/Ped Dental',           num_members,
+              builder.create_kaiser_health('KP DC Bronze 6400/55/Dental/Ped Dental', num_members,
               	deductible: 6400, is_standard_plan: false, metal_level: 'bronze', avg_member_premium: 198.38), 
               
-              builder.create_plan('KP DC Catastrophic 7150/0/Dental/Ped Dental',      num_members,
+              builder.create_kaiser_health('KP DC Catastrophic 7150/0/Dental/Ped Dental', num_members,
               	deductible: 7150, is_standard_plan: false, metal_level: 'catastrophic', avg_member_premium: 173.34),
               
-              builder.create_plan('KP DC Gold 0/20/Dental/Ped Dental',                num_members,
+              builder.create_kaiser_health('KP DC Gold 0/20/Dental/Ped Dental', num_members,
               	deductible: 0, is_standard_plan: false, metal_level: 'gold', avg_member_premium: 342.87), 
               
-              builder.create_plan('KP DC Gold 1000/20/Dental/Ped Dental',             num_members,
+              builder.create_kaiser_health('KP DC Gold 1000/20/Dental/Ped Dental', num_members,
               	deductible: 1000, is_standard_plan: false, metal_level: 'gold', avg_member_premium: 316.93), 
               
-              builder.create_plan('KP DC STANDARD Bronze 5000/50/Dental/Ped Dental',  num_members,
+              builder.create_kaiser_health('KP DC STANDARD Bronze 5000/50/Dental/Ped Dental', num_members,
               	deductible: 5000, metal_level: 'bronze', avg_member_premium: 214.52), 
               
-              builder.create_plan('KP DC STANDARD Gold 500/25/Dental/Ped Dental',     num_members,
+              builder.create_kaiser_health('KP DC STANDARD Gold 500/25/Dental/Ped Dental', num_members,
               	deductible: 500, metal_level: 'gold', avg_member_premium: 339.03),
               
-              builder.create_plan('KP DC STANDARD Platinum 0/20/Dental/Ped Dental',   num_members,
+              builder.create_kaiser_health('KP DC STANDARD Platinum 0/20/Dental/Ped Dental', num_members,
               	deductible: 0, metal_level: 'platinum', avg_member_premium: 380.79), 
               
-              builder.create_plan('KP DC STANDARD Silver 2000/25/Dental/Ped Dental',  num_members,
+              builder.create_kaiser_health('KP DC STANDARD Silver 2000/25/Dental/Ped Dental', num_members,
               	deductible: 2000, metal_level: 'silver', avg_member_premium: 285.76), 
               
-              builder.create_plan('KP DC Silver 1800/30/Dental/Ped Dental',           num_members,
+              builder.create_kaiser_health('KP DC Silver 1800/30/Dental/Ped Dental', num_members,
               	deductible: 1800, is_standard_plan: false, metal_level: 'silver', avg_member_premium: 276.54), 
               
-              builder.create_plan('KP DC Silver 2750/20%/HSA/Dental/Ped Dental',      num_members,
+              builder.create_kaiser_health('KP DC Silver 2750/20%/HSA/Dental/Ped Dental', num_members,
               	deductible: 2750, is_standard_plan: false, metal_level: 'silver', avg_member_premium: 247.44) 
   	]
 
